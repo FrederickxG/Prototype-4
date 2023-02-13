1d945e944b4a8dd0d1428a3d5dfed0fffc90c051
FrederickxG <fgardnerjr24@aoiths.org>
2023-02-13 12:33:31 -0500

    Completed 4.4

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
   public float speed;
   private Rigidbody enemyRb;
   private GameObject player;
+  
 
   void Start()
   {
@@ -16,8 +17,14 @@ public class Enemy : MonoBehaviour
 
   void Update()
   {
-
+    if (transform.position.y < -10) 
+    {
+      Destroy(gameObject);
+    }
     Vector3 lookDirection = (player.transform.position - transform.position).normalized;
     enemyRb.AddForce(lookDirection * speed);
   }
 }
+
+
+
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,12 +6,35 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
+
 
     void Start()
     {
-      Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+      Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+      SpawnEnemyWave(waveNumber);
     }
 
+    void Update()
+    {
+    enemyCount = FindObjectsOfType<Enemy>().Length;
+    if (enemyCount == 0)
+    {
+    Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+       waveNumber++; SpawnEnemyWave(waveNumber);
+    }
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+      for (int i = 0; i < enemiesToSpawn; i++)
+      {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(),
+        enemyPrefab.transform.rotation);
+      }
+    }
+      
     private Vector3 GenerateSpawnPosition()
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
